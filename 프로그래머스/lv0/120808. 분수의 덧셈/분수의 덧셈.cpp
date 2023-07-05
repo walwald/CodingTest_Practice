8dcdd4da0dff889d566adff367f7026dd6c60d4f
Dahee Jang <120387100+walwald@users.noreply.github.com>
2023-07-05 21:25:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 2.70 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
+    vector<int> answer;
+    
+    //통분한 합
+    int numer = numer1 * denom2 + numer2 * denom1;
+    int denom = denom1 * denom2;
+    
+    
+    //최대 공약수로 나누기
+    for(int i = 1000000; i > 0; i--){
+        if(numer % i == 0 && denom % i == 0){
+            numer = numer / i;
+            denom = denom / i;
+        }
+    }
+    
+    answer = { numer, denom };
+    return answer;
+}
