bef2a8f54c8270600d7ccbbb67c6fba16fc2a0dc
Dahee Jang <120387100+walwald@users.noreply.github.com>
2023-07-20 15:50:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,40 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+//next_permutation
 
 vector<int> h(9, 0);
-vector<int> ret(7, 0);
-void change(vector<int> &v){
-    for(int i = 0; i < 7; i++){
-        ret[i] = h[v[i]];
-    }
-    sort(ret.begin(), ret.end());
-    return;
-}
-void combi(int start, vector<int> &v){
-    if(v.size() == 7){
-        change(v);
-        if(accumulate(ret.begin(), ret.end(), 0) == 100){
-            for(int a : ret) cout << a << '\n';
-            exit(0);
-        } 
-        return;
-    }
-    for(int i = start + 1; i < 9; i++){
-        v.push_back(i);
-        combi(start + 1, v);
-        v.pop_back();
-    }
-    return;
-}
-
 
 int main(){
+      
     for(int i = 0; i < 9; i++){
-        cin >> h[i];        
+        cin >> h[i];
     }
-    
-    vector<int> v;
-    combi(-1, v);
+
+    do{
+       int sum = 0;
+        for(int i = 0; i < 7; i++){
+            sum += h[i];
+        }
+        if(sum == 100) break;
+        
+    }while(next_permutation(h.begin(), h.end()));
+        
+    sort(h.begin(), h.begin() + 7);
+    for(int i = 0; i < 7; i++) cout << h[i] << '\n';
     
     return 0;
 }
