80c38d87046214f6081dbb6229c21563623d245d
Dahee Jang <120387100+walwald@users.noreply.github.com>
2023-07-22 21:27:10 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    int answer = 0;
+    for(int i = 1; i <= 50; i++){
+        if(i * slice >= n) {
+            answer = i;
+            break;
+        }
+    }
+
+    return answer;
+}
