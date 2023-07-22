a11400138dfa018df0a094e027bd7a4008f8f9b4
Dahee Jang <120387100+walwald@users.noreply.github.com>
2023-07-22 21:33:59 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    
+    answer = {money / 5500, money % 5500};
+    
+    return answer;
+}
