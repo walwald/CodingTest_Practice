d0ce7802eff6c18f85c17be8ef4eb75424e38a71
Dahee Jang <120387100+walwald@users.noreply.github.com>
2023-08-06 23:21:07 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    answer += hp / 5;
+    answer += hp % 5 / 3;
+    answer += hp % 5 % 3 / 1; 
+    return answer;
+}
