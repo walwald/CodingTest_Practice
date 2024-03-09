6c16f6ffb622f253e59fe633eca9cbb2d55ef493
Dahee Jang <120387100+walwald@users.noreply.github.com>
2024-03-09 12:09:01 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2816 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,47 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-vector<string> ret;
 stack<int> stk;
+bool bb = true;
+vector<char> ret;
 int main(){
-  // ios::sync_with_stdio(false);
-  // cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n;
+    int arr[n];
+    
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    int j = 1;
 
-  cin >> n;
-  vector<int> org(n);
-  vector<int> cp(n);
-  
-  for(int i = 0; i < n; i++){
-    int tmp;
-    cin >> tmp;
-    org[i] = tmp;
-    cp[i] = tmp;
-  }
-
-  // sort(cp, cp + n);
-  sort(cp.begin(), cp.end());
-  // 불가능한 경우가 언제지?
-
-  queue<int> qq;
-  for(int i : cp) {qq.push(i);}
-
-  for(int i = 0; i < n; i++){
-    while(stk.size() == 0 || stk.top() != org[i]){
-      if(qq.size()==0) {
-        cout << "NO" << '\n';
-        return 0;
-      }
-      int tmp = qq.front();
-      stk.push(tmp);
-      ret.push_back("+");
-      qq.pop();
-    } 
-
-      stk.pop();
-      ret.push_back("-");
-  }
-
-  for(string str :ret) cout << str << '\n';
-
-  return 0;
+    
+    for(int i = 0; i < n; i++){
+        while(!stk.size() || stk.top() != arr[i]){
+            // 숫자 비교 주의
+           if(j > n){
+            cout << "NO" << '\n';
+            return 0;
+           }
+            stk.push(j);
+            ret.push_back('+');
+            j++;
+        }
+        
+        if(stk.top() == arr[i]){
+            stk.pop();
+            ret.push_back('-');
+        }    
+    }
+    
+   for(char c : ret) cout << c << '\n';
+    
+    return 0;
 }
